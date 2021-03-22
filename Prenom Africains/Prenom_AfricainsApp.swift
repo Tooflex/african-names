280b74a2cea75664aa9e09664877fed36dd9c541
Otourou Da Costa <otouroudacosta@gmail.com>
2021-03-22 01:07:14 +0100

    Initial Commit

---
+++ Prenom Africains/Prenom_AfricainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prenom_AfricainsApp.swift
+//  Prenom Africains
+//
+//  Created by Otourou Da Costa on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Prenom_AfricainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
