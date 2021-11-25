90de05ae12f2434e86a1d4473bf25ef37b091cac
Otourou Da Costa <otouroudacosta@gmail.com>
2021-11-25 23:48:54 +0100

    Add files deleted by mistake

---
+++ Prenom Africains/ViewModel/ParamViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ParamViewModel.swift
+//  Prenom Africains
+//
+//  Created by Otourou Da Costa on 25/11/2021.
+//
+
+import Foundation
+
+final class ParamViewModel: ObservableObject {
+
+}
